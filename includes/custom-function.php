a0c4d96c0e737cd428260b6e362f1cfed7f22847
aadiityaak <aadiityaak@gmail.com>
2024-08-02 15:12:55 +0700

    init

---
+++ includes/custom-function.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Masukkan semua function tambahan disini
+ *
+ * @link       https://velocitydeveloper.com
+ * @since      1.0.0
+ *
+ * @package    Custom_Plugin
+ * @subpackage Custom_Plugin/includes
+ */
---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+/*!
+  *  v1.0.2 (https://velocitydeveloper.com)
+  * Copyright 2013-2024 Velocity Developer
+  * Licensed under GPL (http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+  */
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+})((function () { 'use strict';
+
+
+
+}));
+//# sourceMappingURL=script.js.map
