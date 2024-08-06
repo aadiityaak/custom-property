93daa2e7ca710ae0b815857d881fb9557925b453
Iimamtzh <iimamtzhthriiee21@gmail.com>
2024-08-06 21:53:19 +0700

    Perbaiki tombol dan sedikit function list property

--- includes/cmb2-frontend-property-submit.php
+++ includes/cmb2-frontend-property-submit.php
@@ -152,6 +152,7 @@ class CMB2_Frontend_Form_Bs
         );
 
         $query = new WP_Query($args);
+        ob_start();
 
         // The Loop
 ?>
@@ -181,10 +182,31 @@ class CMB2_Frontend_Form_Bs
                         <tr>
                             <th scope="row"><?php the_ID(); ?></th>
                             <td><?php the_title(); ?></td>
+                            <td>Rp.
+                                <?php
+                                $harga = get_user_meta(get_current_user_id(), 'cp_harga', true);
+                                if (!empty($harga)) {
+                                    echo number_format(floatval($harga), 0, ',', '.');
+                                } else {
+                                    echo '0';
+                                }
+                                ?>
+                            </td>
                             <td>
-                                <a href="<?php the_permalink(); ?>" class="btn btn-primary">View</a>
-                                <a href="<?php echo get_the_permalink($submit_page); ?>?post_id=<?php the_ID(); ?>" class="btn btn-warning">Edit</a>
-                                <a href="<?php echo $delete_url; ?>" class="btn btn-danger">Delete</a>
+                                <div class="btn-group" role="group" aria-label="Basic example">
+                                    <a href="<?php the_permalink(); ?>" class="btn btn-primary btn-sm"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+                                            <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1 1 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4 4 0 0 1-.128-1.287z" />
+                                            <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243z" />
+                                        </svg></a>
+                                    <a href="<?php echo get_the_permalink($submit_page); ?>?post_id=<?php the_ID(); ?>" class="btn btn-warning btn-sm"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
+                                        </svg></a>
+                                    <a href="<?php echo $delete_url; ?>" class="btn btn-danger btn-sm"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                                        </svg></a>
+                                </div>
                             </td>
                         </tr>
                 <?php
@@ -213,6 +235,8 @@ class CMB2_Frontend_Form_Bs
             </nav>
 <?php
         }
+
+        return ob_get_clean();
     }
 
     public function register_property_frontend_form()
